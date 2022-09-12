b0b9a8d77509da2fb2b6e206f5645e6bdbc662b8
Anastasia <anastasiauneauretour@yandex.ru>
2022-09-12 22:00:34 +0900

    Task2_Seminar4

--- Seminar4/Task1/Program.cs
+++ Seminar4/Task1/Program.cs
@@ -3,4 +3,20 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+System.Console.WriteLine("Здравствуйте! Я могу возвести число А в натуральную степень В.");
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+Number(A,B);
+
+void Number (int A, int B)
+{
+    int index=1;
+    while (index>0)
+    {
+        System.Console.WriteLine("A^B =" + Math.Pow(A,B));
+        break;
+    }
+    Console.ReadKey();
+}     
 
---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Здравствуйте! Я могу назвать сумму цифр в вашем числе.");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Number(sum);
+
+void Number(int sum)
+{
+    while(num>0)
+    {
+        sum=sum+num%10;
+        num=num/10;       
+    }
+    Console.WriteLine(sum);
+    Console.ReadKey();
+}
+
