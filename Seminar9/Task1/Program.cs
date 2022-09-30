46da77115d4c4f8d7298113cfb6a2f30c74009ec
Anastasia <anastasiauneauretour@yandex.ru>
2022-10-01 03:19:06 +0900

    Task2

--- Seminar9/Task1/Program.cs
+++ Seminar9/Task1/Program.cs
@@ -3,7 +3,7 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-System.Console.WriteLine("Программа, которая выводитвсе натуральные числа от N до 1");
+System.Console.WriteLine("Программа, которая выводит все натуральные числа от N до 1");
 System.Console.WriteLine("Введите N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine(NaturalNumbers(N));
---
+++ Seminar9/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.WriteLine("Программа, которая находит сумму натуральных элементов от M до N");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumNaturalNumbers(M, N));
+
+int SumNaturalNumbers(int M, int N)
+{
+    if (N == M)
+        return M;
+      return N + SumNaturalNumbers(M, N-1);
+}
